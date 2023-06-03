2dc28398321844413103551755e726466c974d3d
carolinabispo <carolinabispos07@gmail.com>
2023-06-02 21:19:49 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import HomeExercicios from "./exercicios/HomeExercicios";
+import HomePage from "./components/HomePage";
+
+
+function App() {
+  return (
+    
+    <div>
+      {/* <HomeExercicios /> */}
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/data.js
@@ -0,0 +1,113 @@
+import nave1 from "../utils/nave-1.jpg";
+import nave2 from "../utils/nave-2.jpg";
+import nave3 from "../utils/nave-3.jpg";
+import nave4 from "../utils/nave-4.jpg";
+import foguete1 from "../utils/foguete-1.jpg";
+import foguete2 from "../utils/foguete-2.jpg";
+import foguete3 from "../utils/foguete-3.jpg";
+import foguete4 from "../utils/foguete-4.jpg";
+import caneca1 from "../utils/caneca-1.jpg";
+import caneca2 from "../utils/caneca-2.jpg";
+
+const db = [
+  {
+    name: "Miniatura Nave Espacial R.R.W. Vastam - Nº 06 - Star Trek Online - Eaglemoss",
+    id: 1,
+    category: "Nave Espacial",
+    price: 69.99,
+    storage: 3,
+    description:
+      "Um dos cruzadores de batalha de comando Nível 6, o Comando Warbird da classe Vastam Romulan junta-se a batalha e à nossa coleção de naves estelar Star Trek Online! O Command Warbird da classe Romulan Vastam é uma nave iminente disponível para jogadores Romulanos em Star Trek Online. O Comando Tático Romulano da Classe Vastam Warbird atua como um centro de comando móvel, inspirando aliados com atualizações instantâneas de informações ao longo de cada batalha. Construída para satisfazer o desejo romulano de grande escala, a classe Vastam é um Warbird grande e resistente, capaz de liderar uma frota pela frente. A classe recebeu o nome de Vastam Peaks, uma região na superfície da colônia romulana recém-instalada de Mol'Rihan. Seu modelo do Command Warbird da classe Romulan Vastam foi cuidadosamente recriada em molde com extraordinária atenção aos detalhes para trazer a nave da Frota Estelar jogável para sua casa. Material: Metal.\n Acompanha base.\n Dimensões aproximadas: 10,5 x 9,5 cm (Comp x Larg).\n OBS: recomendável para maiores de 4 anos.",
+    image: nave1,
+  },
+  {
+    name: "Miniatura Nave Espacial Modern Raptor",
+    id: 2,
+    category: "Nave Espacial",
+    price: 149.99,
+    storage: 10,
+    description:
+      'Miniatura Nave Espacial Modern Raptor, da série Battlestar Galactica, da linha Official Ships Collection, Eaglemoss. A próxima edição da coleção de naves Battlestar Galactica é o Raptor, um das naves mais versáteis do Exército Colonial. Operando de maneira ofensiva e defensiva, a Raptor foi usado para transportar tropas e munições, aumentar ou bloquear as comunicações e conduzir operações de busca e salvamento. Este modelo de nave Battlestar Galactica Raptor foi criado em metal fundido pesado e materiais ABS de alta qualidade. Ele captura o design exclusivo da nave, uma aparência robusta que recebeu o apelido de "casamata voadora" e pequenos detalhes como luzes de pouso, queima do motor e decalques da cabine. Trabalhamos em estreita colaboração com os membros da equipe Galactica VFX para garantir que o modelo seja o mais preciso possível. A nave vem completa com uma revista de colecionador de 16 páginas, fornecendo um perfil aprofundado da Raptor, artigos exclusivos dos bastidores, esboços de design original e entrevistas.\n Material: Metal.\n Acompanha base.\n Dimensões aproximadas: 23,5 cm x 14,0 cm (Comp x Larg).\n OBS: recomendável para maiores de 4 anos.',
+    image: nave2,
+  },
+  {
+    name: "Miniatura Nave Espacial Jem'Hadar Vanguard Carrier - Nº 08 - Star Trek Online - Eaglemoss",
+    id: 3,
+    category: "Nave Espacial",
+    price: 199.99,
+    storage: 60,
+    description:
+      "Miniatura Nave Espacial Jem'Hadar Vanguard Carrierr, Nº 08, do jogo Star Trek. O Jem'Hadar Vanguard Carrier foi o maior dos navios da nova frota que foi introduzida com a expansão temática do DS9 intitulada 'Victory is Life'. O Vanguard-Class Jem'Hadar Carrier atua como uma base móvel de assalto para o lançamento e reparo de sua frota acompanhante, com um longo convés de voo dorsal a partir do qual duas asas de canhoneiras podem ser lançadas.\n Material: Metal. Acompanha base.\n Dimensões aproximadas: 13,5 x 6,0 cm (Comp x Larg).\n OBS: recomendável para maiores de 4 anos.",
+    image: nave3,
+  },
+  {
+    name: "Miniatura Nave Espacial I.K.S. Mogh - Nº 10 - Star Trek Online - Eaglemoss",
+    id: 4,
+    category: "Nave Espacial",
+    price: 259.99,
+    storage: 68,
+    description:
+      "Miniatura Nave Espacial I.K.S. Mogh, Nº 10, do jogo Star Trek. Uma nave ágil disponível para jogadores Klingon em Star Trek Online, o Mogh-Class Battlecruiser foi projetada por uma equipe científica liderada pelo Embaixador Worf, e nomeado em homenagem a seu pai Mogh. A versátil classe Mogh possui um Sistema de Implantação de Defesa Dinâmica para ajudar no equilíbrio entre ataque e defesa. Seu modelo do Klingon Battlecruiser classe Mogh foi cuidadosamente recriado em molde com extraordinária atenção aos detalhes para trazer a nave Klingon online para sua casa. O perfil agressivo do Mogh segue dicas de design da nave Klingon recentes, como a classe Bortasqu, com pontas de armas claramente visíveis em seu casco - mais notavelmente a grande ponta de arma no centro da nave.\n Material: Metal. Acompanha base.\n Dimensões aproximadas: 12,0 x 9,0 cm (Comp x Larg).\n OBS: recomendável para maiores de 4 anos.",
+    image: nave4,
+  },
+  {
+    name: "Plataforma de Lançamento Ônibus Espacial Premium Series Kit de Montar de Metal - Metal Earth",
+    id: 5,
+    category: "Foguetes",
+    price: 179.9,
+    storage: 15,
+    description:
+      "Com mais de 15 andares de altura, o tanque extra de combustível com mais dois foguetes boosters lateriais, essa é a sua chance de ter um modelo da plataforma completa que lançava os ônibus espaciais para fora da gravidade da Terra. Os dois foguetes boosters queimam 9 toneladas de combustível por segundo, numa velocidade de 4.600 Km por hora! Novamente, trata-se de um KIT DE MONTAR, a peça não vem montada e acabada como nas fotos, o que você vai receber é a embalagem do kit com folhas de metal com as partes destacáveis para você seguir as instruções e montar a peça. O tamanho final dele após montado é de 9 x 10,49 x 17,02 cm (comprimento, largura e altura). A Metal Earth classifica seus kits por níveis de dificuldade, esse kit tem a classificação de Moderado. O kit vem com 4 plaquinhas diferentes, assim você pode montar o ônibus espacial com o nome que você mais gosta: Atlantis, Discovery, Endeavor ou Enterprise. ",
+    image: foguete1,
+  },
+  {
+    name: "Kit Foguetes Históricos Gigantes Da Astronáutica",
+    id: 6,
+    category: "Foguetes",
+    price: 279.8,
+    storage: 5,
+    description:
+      "O kit é composto por:\n 1 ônibus Espacial - 27 cm\n 1 foguete Saturno V - 35 cm\n 1 SLS Ártemis - 42 cm Feitos de plástico com finalidades decorativas",
+    image: foguete2,
+  },
+  {
+    name: "Miniatura Colecionável Foguete SLS/Artemis da NASA",
+    id: 7,
+    category: "Foguetes",
+    price: 165.9,
+    storage: 9,
+    description:
+      "Produzido em PLA de alta qualidade em 6 cores. Conteúdo: SLS com boosters laterais e espaçonave Órion Desmontáveis. Suporte de parede. Embalagem premium e colecionável contendo informações sobre o foguete e o Programa Artemis. Altura: 70 cm",
+    image: foguete3,
+  },
+  {
+    name: "Miniatura Foguete Falcon 9 - SpaceX Escala 1:110",
+    id: 8,
+    category: "Foguete",
+    price: 189.9,
+    storage: 19,
+    description:
+      "Conteúdo da embalagem: Booster do Falcon 9 Segundo Estágio do Falcon 9 Carga de satélites Par de coifas Espaçonave Dragon e Módulo de Serviço Suporte de parede Guia de montagem Peças extras Embalagem personalizada e colecionável. Altura: 63 cm (sem base de apoio)",
+    image: foguete4,
+  },
+  {
+    name: "Caneca Astronauta Fome Infinita",
+    id: 9,
+    category: "Canecas",
+    storage: 35,
+    description:
+      "Caneca Astronauta Fome Infinita Produzidas em porcelana de alta resistência, são ótimas para os momentos de relaxamento ou durante o trabalho. Além disso, você pode utilizá-las também como item de decoração, porta canetas, porta treco, porta objetos ou do jeito que sua imaginação permitir. Com certeza sua mesa de trabalho vai ganhar um toque diferenciado. A estampa possui qualidade digital, cores brilhantes e acabamento impecável. Com alça e bordas arredondadas possibilita um encaixe perfeito para suas mãos, maior conforto na hora de degustar suas bebidas prediletas. Totalmente resistentes a micro-ondas e lava-louças. Podem ser aquecidas e lavadas que não perdem em nada a qualidade da porcelana e da impressão. É uma excelente opção para você presentear quem você quiser. Tamanho: Altura: 9,5cm Diâmetro: 8cm Capacidade: 325ml",
+    image: caneca1,
+  },
+  {
+    name: "Caneca Alien Humanos Viemos em Pás",
+    id: 10,
+    category: "Canecas",
+    storage: 29,
+    description:
+      "Produzidas em porcelana de alta resistência, são ótimas para os momentos de relaxamento ou durante o trabalho. Além disso, você pode utilizá-las também como item de decoração, porta canetas, porta treco, porta objetos ou do jeito que sua imaginação permitir. Com certeza sua mesa de trabalho vai ganhar um toque diferenciado. A estampa possui qualidade digital, cores brilhantes e acabamento impecável. Com alça e bordas arredondadas possibilita um encaixe perfeito para suas mãos, maior conforto na hora de degustar suas bebidas prediletas. Totalmente resistentes a micro-ondas e lava-louças. Podem ser aquecidas e lavadas que não perdem em nada a qualidade da porcelana e da impressão. É uma excelente opção para você presentear quem você quiser. Tamanho: Altura: 9,5cm Diâmetro: 8cm Capacidade: 325ml",
+    image: caneca2,
+  },
+];
+
+export default db;
